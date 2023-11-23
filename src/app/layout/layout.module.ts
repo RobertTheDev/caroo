5c9a4b87e86db84043a40771b7ab9cfd9e83085c
RobertTheDev <robertthedev@gmail.com>
2023-11-23 10:02:47 +0000

    feat: added layout components

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,10 +4,17 @@ import { CommonModule } from '@angular/common';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderProfileMenuComponent } from './header-profile-menu/header-profile-menu.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, PageLayoutComponent],
-  imports: [CommonModule],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    PageLayoutComponent,
+    HeaderProfileMenuComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [PageLayoutComponent],
 })
 export class LayoutModule {}
