a536b360e6d5e3755c4fd0e192e4b009bd6369b0
RobertTheDev <robertthedev@gmail.com>
2023-11-23 12:29:21 +0000

    feat: added ng optimized image

--- src/app/car/car.module.ts
+++ src/app/car/car.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarCardComponent } from './car-card/car-card.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [CarCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   exports: [CarCardComponent],
 })
 export class CarModule {}
