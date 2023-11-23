f27c61d22a2ea9b40ef783a78bb067f4aa472e5e
RobertTheDev <robertthedev@gmail.com>
2023-11-23 12:29:58 +0000

    feat: added auth components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SignUpFormComponent, SignInFormComponent],
   imports: [CommonModule],
 })
 export class AuthModule {}
