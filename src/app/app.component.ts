8ee10fcff6ecad4da20a73a38c36dea1526480b7
RobertTheDev <robertthedev@gmail.com>
2023-11-23 02:47:09 +0000

    feat: mapped cars data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LayoutModule } from './layout/layout.module';
+import { CarModule } from './car/car.module';
+import carsData from './data/carsData';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LayoutModule],
+  imports: [CommonModule, RouterOutlet, LayoutModule, CarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'caroo';
+
+  carsData = carsData;
 }
