3657ce45a1279d25959a7d702577eefb507df84a
RobertTheDev <robertthedev@gmail.com>
2023-11-23 10:01:15 +0000

    feat: created footer links

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import companyName from '../../lib/constants/companyName';
+import currentYear from '../../lib/constants/currentYear';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  companyName = companyName;
+
+  currentYear = currentYear;
+
+  footerLinks = [
+    {
+      url: '/about',
+      name: 'About',
+    },
+    {
+      url: '/accessibility-statement',
+      name: 'Accessibility Statement',
+    },
+    {
+      url: '/privacy-policy',
+      name: 'Privacy Policy',
+    },
+    {
+      url: '/terms-and-conditions',
+      name: 'Terms and Conditions',
+    },
+  ];
+}
