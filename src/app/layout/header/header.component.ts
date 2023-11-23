d8b395a0702d6dd91edd1dbf0d017e5c0fac7a4b
RobertTheDev <robertthedev@gmail.com>
2023-11-23 10:01:38 +0000

    feat: added header icons

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import companyName from '../../lib/constants/companyName';
+import {
+  faBars,
+  faBell,
+  faHeart,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  companyName = companyName;
+
+  faBars = faBars;
+  faBell = faBell;
+  faHeart = faHeart;
+  faUser = faUser;
+
+  headerLinks = [
+    {
+      href: '/',
+      name: 'Home',
+    },
+    {
+      href: '/about',
+      name: 'About',
+    },
+    {
+      href: '/find-cars',
+      name: 'Find Cars',
+    },
+    {
+      href: '/sell',
+      name: 'Sell',
+    },
+  ];
+}
